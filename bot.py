dd1b8f865db98971a39d77b6e6cb321b47d554f6
Tyler Bonnell <tbonnell@uw.edu>
2016-03-19 01:14:56 -0700

    something I guess

--- bot.py
+++ bot.py
@@ -1,4 +1,9 @@
 import urllib2
 import json
-term = 'seattle'
-print json.load(urllib2.urlopen("http://api.urbandictionary.com/v0/define?term=" + term))["list"][0]
+term = "yeltsin"
+term = term.replace(" ", "%20")
+req = json.load(urllib2.urlopen("http://api.urbandictionary.com/v0/define?term=" + term))["list"]
+if len(req) > 0:
+    print req[0]
+else:
+    print "No results found!"
